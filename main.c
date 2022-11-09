0b18f66110432bed1cb928af9e4514594fb54188
= <baloaloajansen@gmail.com>
2022-11-09 19:46:06 +0800

    Implemented the Propagate Functions

--- main.c
+++ main.c
@@ -1,14 +1,17 @@
 #include <stdio.h>
 
 void PrintTable(int* table);
+void ParseTable(int* table);
+void Propagate(int xPos, int yPos, int value, void* items);
 
-//Wave Function Collapse
-struct item
+
+typedef struct
 {
-	bool isSuperposition = true;
-	int[9] posibleStates;
-	int length;
-}
+    int posibleStates[9];
+    int length;
+
+} Superposition;
+
 
 int main()
 {   
@@ -26,7 +29,10 @@ int main()
     	{1,5,0, 0,0,6, 4,0,9}
 	};
 
+	ParseTable(table);
     PrintTable(table);
+
+    getchar(); //Not required but program exits immediately on Visual Studio without it
 }
 
 
@@ -57,18 +63,120 @@ void PrintTable(int *table)
     }
 }
 
-void ParseTable()
+
+void ParseTable(int* table)
 {
+    Superposition items[9][9];
+    int superPositionsLeft = 81;
+
 	//Create items for all and collapse those which are now given,
-	printf("Table parsed");
+    for (int y = 0; y < 9; y++)
+    {
+        for (int x = 0; x < 9; x++)
+        {
+            //read the table
+            int value = table[y * 9 + x];
+            
+            if (value != 0)
+            {
+                superPositionsLeft -= 1;
+                Propagate(x, y, value, items);
+                continue;
+            }
+
+            //set superposition defaults
+            items[x][y].length = 9;
+
+            //Dirty?
+            for (int i = 0; i < 9; i++)
+            {
+                items[x][y].posibleStates[i] = i;
+            }
+        }
+    }
+    printf("Number of superpositions left: %d\n", superPositionsLeft);
 }
+
 
 void Collapse()
 {
 	
 }
 
-void Propagate()
+
+void _PropagateBox(int xPos, int yPos, int value, Superposition* items)
 {
-	
+    //get box id
+    int xBox = xPos / 3;
+    int yBox = yPos / 3;
+
+    //Get components in the box,
+    for (int y = 0; y < 3; y++)
+    {
+        for (int x = 0; x < 3; x++)
+        {
+            int xPos = x + xBox;
+            int yPos = y + yBox;
+
+            //Get Item
+            Superposition* item = items + yPos * 9 + xPos;
+
+            for (int i = 0; i < item->length; i++)
+            {
+                if (item->posibleStates[i] == value)
+                {
+                    item->posibleStates[i] = -1; // -1 means invalid
+                    item->length -= 1;
+                    break;
+                }
+            }
+        }
+    }
+    //Update each component's possible states
+}
+
+
+void _PropagateRow(int yPos, int value, Superposition* items)
+{
+    for (int x = 0; x < 9; x++)
+    {
+        Superposition* item = items + yPos * 9 + x;
+
+        for (int i = 0; i < item->length; i++)
+        {
+            if (item->posibleStates[i] == value)
+            {
+                item->posibleStates[i] = -1; // -1 means invalid
+                item->length -= 1;
+                break;
+            }
+        }
+    }
+}
+
+
+void _PropagateColumn(int xPos, int value, Superposition* items)
+{
+    for (int y = 0; y < 9; y++)
+    {
+        Superposition* item = items + y * 9 + xPos;
+
+        for (int i = 0; i < item->length; i++)
+        {
+            if (item->posibleStates[i] == value)
+            {
+                item->posibleStates[i] = -1; // -1 means invalid
+                item->length -= 1;
+                break;
+            }
+        }
+    }
+}
+
+
+void Propagate(int xPos, int yPos, int value, Superposition* items)
+{
+    _PropagateBox(xPos, yPos, value, items);
+    _PropagateRow(yPos, value, items);
+    _PropagateColumn(xPos, value, items);
 }
